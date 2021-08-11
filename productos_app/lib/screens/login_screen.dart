915c6d7b0b486431975dd8c46cc9f4c69b8b7174
Bryan <brayannhc07@gmail.com>
2021-08-11 14:22:06 -0500

    Sección 12: fin de seccion

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/providers/login_form_provider.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -20,7 +23,10 @@ class LoginScreen extends StatelessWidget {
                 ),
                 Text("Login", style: Theme.of(context).textTheme.headline4),
                 SizedBox(height: 30),
-                _LoginForm()
+                ChangeNotifierProvider(
+                  create: (_) => LoginFormProvider(),
+                  child: _LoginForm(),
+                ),
               ],
             ),
           ),
@@ -28,6 +34,9 @@ class LoginScreen extends StatelessWidget {
           Text(
             "Crear una nueva cuenta",
             style: Theme.of(context).textTheme.subtitle1,
+          ),
+          SizedBox(
+            height: 50,
           )
         ],
       ),
@@ -38,30 +47,81 @@ class LoginScreen extends StatelessWidget {
 class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
     return Container(
       child: Form(
+          key: loginForm.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepPurple),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'john.doe@gmail.com',
+                    labelText: "Correo electrónico",
+                    prefixIcon: Icons.alternate_email_rounded),
+                onChanged: (value) => loginForm.email = value,
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+                  RegExp regExp = new RegExp(pattern);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El valor ingresado no luce como un correo. ';
+                },
               ),
-              focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
-              hintText: "ejemplo@correo.com",
-              labelText: "Correo electrónico",
-              labelStyle: TextStyle(color: Colors.grey),
-              prefixIcon: Icon(
-                Icons.alternate_email_sharp,
-                color: Colors.deepPurple,
+              SizedBox(
+                height: 10,
               ),
-            ),
-          )
-        ],
-      )),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '********',
+                    labelText: "Contraseña",
+                    prefixIcon: Icons.lock_outline),
+                onChanged: (value) => loginForm.password = value,
+                validator: (value) {
+                  return value != null && value.length >= 6
+                      ? null
+                      : 'La contraseña debe ser de 6 caracteres.';
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  disabledColor: Colors.grey,
+                  elevation: 0,
+                  color: Colors.deepPurple,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    child: Text(
+                      loginForm.isLoading ? "Espere..." : "Ingresar",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  onPressed: loginForm.isLoading
+                      ? null
+                      : () async {
+                          FocusScope.of(context).unfocus();
+
+                          if (loginForm.isValidForm()) {
+                            loginForm.isLoading = true;
+
+                            await Future.delayed(Duration(seconds: 2));
+
+                            loginForm.isLoading = false;
+
+                            Navigator.pushReplacementNamed(context, 'home');
+                          }
+                        })
+            ],
+          )),
     );
   }
 }
--- productos_app/lib/widgets/auth_backgroud.dart
+++ productos_app/lib/widgets/auth_backgroud.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AuthBackground extends StatelessWidget {
   final Widget child;
 
-  const AuthBackground({Key key, @required this.child}) : super(key: key);
+  const AuthBackground({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
