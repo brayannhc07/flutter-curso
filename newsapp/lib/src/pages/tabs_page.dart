bca32526f6abc5efb784ff5668281a37031d2767
Bryan <brayannhc07@gmail.com>
2021-11-08 10:49:56 -0600

    Seccion 18: Inicio seccion

---
+++ newsapp/lib/src/pages/tabs_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class TabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => new _NavegacionModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline), label: "Para Ti"),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: "Encabezados")
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: [Container(color: Colors.red), Container(color: Colors.green)],
+    );
+  }
+}
+
+class _NavegacionModel with ChangeNotifier {
+  int _paginaActual = 0;
+
+  int get paginaActual => _paginaActual;
+  set paginaActual(int valor) {
+    this._paginaActual = valor;
+    notifyListeners();
+  }
+}
